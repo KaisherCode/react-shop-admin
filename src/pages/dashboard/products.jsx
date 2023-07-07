9cb5bc3f72fc3c4335ebb668378cdf2477c7bcf9
Wilson Dev <wilson.hch@hynkor.com>
2023-07-07 10:14:45 -0500

    Creación del componente Alert

--- src/pages/dashboard/products.jsx
+++ src/pages/dashboard/products.jsx
@@ -1,13 +1,32 @@
-import {useState } from "react"
+import { useState, useEffect } from "react"
 import { PlusIcon } from "@heroicons/react/solid"
 import Modal from "@common/Modal"
 import FormProduct from "@components/FormProduct"
+import axios from "axios"
+import endPoints from "@services/api"
+import useAlert from "@hooks/useAlert"
+import Alert from "@common/Alert"
 
-export default function products() {
+export default function Products() {
     const [open, setOpen] = useState(false)
     const [products, setProducts] = useState([])
+    const { alert, setAlert, toggleAlert } = useAlert()
+
+    useEffect(() => {
+        async function getProducts() {
+            const response = await axios.get(endPoints.products.allproducts)
+            setProducts(response.data)
+        }
+        try {
+            getProducts()
+        } catch {
+            console.log(error)
+        }
+    }, [])
+
     return (
         <>
+            <Alert alert={alert} handleClose={toggleAlert} />
             <div className="lg:flex lg:items-center lg:justify-between mb-8">
                 <div className="min-w-0 flex-1">
                     <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
@@ -20,7 +39,7 @@ export default function products() {
                         <button
                             type="button"
                             className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                            onClick={()=>setOpen(true)}
+                            onClick={() => setOpen(true)}
                         >
                             <PlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
                             Add Product
@@ -86,7 +105,7 @@ export default function products() {
                     </div>
                 </div>
                 <Modal open={open} setOpen={setOpen}>
-                    <FormProduct/>
+                    <FormProduct setOpen={setOpen} setAlert={setAlert} />
                 </Modal>
             </div>
         </>
